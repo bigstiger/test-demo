bb302bca988dde6584ea9b4e3feb74e07b180781
zheng.yuan.o <m13052908568_1@163.com>
2019-03-25 15:22:54 +0800

    03.25

--- thread-demo/src/main/java/com/stiger/threadOne/myThread.java
+++ thread-demo/src/main/java/com/stiger/threadOne/myThread.java
@@ -1,5 +1,7 @@
 package com.stiger.threadOne;
 
+import java.util.Date;
+
 /**
  * @author YZ
  * @create 2019-03-23-19:51
@@ -9,7 +11,15 @@ public class myThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i <= 100; i++){
-            System.out.println(getName() + "---" + i);
+            System.out.println(getName() + "---" + i + new Date());
+            try {
+                //线程睡眠,但不太精确,受电脑性能的影响
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //线程礼让:一定程度上让线程执行的更和谐(cpu的使用时间更均匀),但不能保证完全一致
+            //Thread.yield();
         }
     }
 }
--- thread-demo/src/main/java/com/stiger/threadOne/myThreadDemo.java
+++ thread-demo/src/main/java/com/stiger/threadOne/myThreadDemo.java
@@ -9,8 +9,27 @@ public class myThreadDemo {
     public static void main(String[] args) {
         myThread my1 = new myThread();
         myThread my2 = new myThread();
+        myThread my3 = new myThread();
 
-        my1.start();
+        //给线程起名字
+        my1.setName("线程1");
+        my2.setName("线程2");
+        my3.setName("线程3");
+
+        //设置线程优先级,最大10,最小1,默认5
+        //my1.setPriority(10);
+        //my2.setPriority(1);
+
+        //my1.start();
+        //线程加入,写在其他线程start之前,当前线程执行完成再执行下面线程
+        try {
+            my1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         my2.start();
+        my3.start();
+
+        //System.out.println("主线程:" + Thread.currentThread().getName());
     }
 }
